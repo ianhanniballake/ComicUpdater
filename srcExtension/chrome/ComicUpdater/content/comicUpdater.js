1f27146f185ed7ad8b44010b0a3175ba75ce2e0a
Ian Lake <ian.hannibal.lake@gmail.com>
2010-02-21 16:50:49 -0500

    Initial Commit
    Signed-off-by: Ian Lake <ian.hannibal.lake@gmail.com>

---
+++ srcExtension/chrome/ComicUpdater/content/comicUpdater.js
@@ -0,0 +1,74 @@
+var comicUpdater = function () {
+    var historyService = Components.classes["@mozilla.org/browser/nav-history-service;1"]
+                                            .getService(Components.interfaces.nsINavHistoryService);
+    var bookmarksService = Components.classes["@mozilla.org/browser/nav-bookmarks-service;1"]
+                                              .getService(Components.interfaces.nsINavBookmarksService);
+    var ios = Components.classes["@mozilla.org/network/io-service;1"]
+                                 .getService(Components.interfaces.nsIIOService);
+    return {
+        run : function () {
+            var options = historyService.getNewQueryOptions();
+            var query = historyService.getNewQuery();
+            var bookmarkMenuFolder = bookmarksService.bookmarksMenuFolder;
+            
+            var folderList = new Array();
+            var maxMatchingChars = -1;
+            var closestBookmarkList = new Array();
+            folderList.push(bookmarkMenuFolder);
+            
+            while (folderList.length > 0)
+            {
+                query.setFolders([folderList.pop()], 1);
+                var result = historyService.executeQuery(query, options);
+                var rootNode = result.root;
+                rootNode.containerOpen = true;
+                for (var i = 0; i < rootNode.childCount; i ++)
+                {
+                    var node = rootNode.getChild(i);
+                    if (node.type == node.RESULT_TYPE_FOLDER)
+                    {
+                        folderList.push(node.itemId);
+                    }
+                    else
+                    {
+                        var numMatchingChars = 0;
+                        for (var j=1; j<node.uri.length && j<content.document.URL.length; j++)
+                        {
+                            if (node.uri.substr(0,j)==content.document.URL.substr(0,j))
+                                numMatchingChars = j;
+                            else
+                                break;
+                        }
+                        if (numMatchingChars > maxMatchingChars)
+                        {
+                            closestBookmarkList = new Array();
+                            maxMatchingChars = numMatchingChars;
+                        }
+                        if (numMatchingChars >= maxMatchingChars)
+                        {
+                            closestBookmarkList.push(node);
+                        }
+                    }
+                }
+
+                // close a container after using it!
+                rootNode.containerOpen = false;
+            }
+            
+            if (maxMatchingChars < 10)
+                alert("Could not find any bookmark that shares at least 10 characters!");
+            else if (closestBookmarkList.length == 1)
+            {
+                var newURI = ios.newURI(content.document.URL, null, null);
+                bookmarksService.changeBookmarkURI(closestBookmarkList[0].itemId, newURI);
+            }
+            else
+            {
+                var nodeList = "";
+                for (var h=0; h<closestBookmarkList.length; h++)
+                    nodeList = nodeList + "  " + closestBookmarkList[h].uri + "\n";
+                alert("Could not find a unique closest bookmark. Found " + closestBookmarkList.length + " closest bookmarks:\n" + nodeList);
+            }
+        }
+    };
+}();
